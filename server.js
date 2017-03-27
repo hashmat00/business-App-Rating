3dacca1fa41424113864de394594da3537c1b564
hashmat00 <info@hashmat00.com>
2017-03-27 22:49:01 +0000

    added user models

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ var engine        = require("ejs-mate");
 var mongoose      = require("mongoose");
 var session       = require("express-session");
 var MongoStore    = require("connect-mongo")(session);
-
+var passport      = require("passport");
+var flash         = require("connect-flash");
 
 var app        = express();
+
+
+//========== CONNECT DATABASE ========================
 mongoose.connect('mongodb://localhost/business');
 
+require('./config/passport');
 
+
+//==========MIDDLE WARE ========================
 
 app.use(express.static('public'));
 app.engine('ejs', engine);
@@ -32,8 +39,12 @@ app.use(require("express-session")({
   store: new  MongoStore({mongooseConnection: mongoose.connection})
 }));
 
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
+//==========MIDDLE WARE ========================
 
 
 // ====== ROUTES ===========
@@ -46,6 +57,10 @@ require('./routes/user')(app);
 
 
 
+
+
+
+//============= RUN SERVER ========================
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
   // var addr = app.address();
   // console.log("Chat server listening at", addr.address + ":" + addr.port);
