97ca7d8adac4e97ed832a61ee29ee75c7181d9e7
hashmat00 <info@hashmat00.com>
2017-03-30 06:44:07 +0000

    added sign up error handling on user.js and signup page

--- routes/user.js
+++ routes/user.js
@@ -11,11 +11,13 @@ module.exports = (app, passport) => {
     });
     
     
-     app.get('/signup', (req, res, next) => {
-        res.render('user/signup', {title: 'SignUp || Reate Me'});
+  app.get('/signup', (req, res) => {
+        var errors = req.flash('error');
+      console.log(errors);
+        res.render('user/signup', {title: 'Sign Up || RateMe', messages: errors, hasErrors: errors.length > 0});
     });
     
-    app.post('/signup', passport.authenticate('local.signup', {
+    app.post('/signup',validate, passport.authenticate('local.signup', {
         successRedirect: '/',
         failureRedirect: '/signup',
         failureFlash: true
@@ -25,3 +27,26 @@ module.exports = (app, passport) => {
 };
 
 
+function validate(req, res, next){
+   req.checkBody('fullname', 'Fullname is Required').notEmpty();
+   req.checkBody('fullname', 'Fullname Must Not Be Less Than 5').isLength({min:5});
+   req.checkBody('email', 'Email is Required').notEmpty();
+   req.checkBody('email', 'Email is Invalid').isEmail();
+   req.checkBody('password', 'Password is Required').notEmpty();
+   req.checkBody('password', 'Password Must Not Be Less Than 5').isLength({min:5});
+   req.check("password", "Password Must Contain at least 1 Number.").matches(/^(?=.*\d)(?=.*[a-z])[0-9a-z]{5,}$/, "i");
+
+   var errors = req.validationErrors();
+
+   if(errors){
+       var messages = [];
+       errors.forEach((error) => {
+           messages.push(error.msg);
+       });
+
+       req.flash('error', messages);
+       res.redirect('/signup');
+   }else{
+       return next();
+   }
+}
